cf62b2edb088d51ff0273dc53db5cfbc0fff9931
hexi <xi.he@xianghuanji.com>
2018-04-30 13:39:37 +0800

    test

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=test',
+    'dsn' => 'mysql:host=localhost;dbname=test',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
